3fb0d2c2491bf44fa5ebe3bfb9b6e9f038bc1e4e
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-07-23 00:27:35 -0700

    Initial commit of ADC driver

---
+++ src/ads1232.py
@@ -0,0 +1,60 @@
+import bitbangio
+import digitalio
+import busio
+import time
+
+
+GAIN_1 = 0b00
+GAIN_2 = 0b10
+GAIN_64 = 0b01
+GAIN_128 = 0b11
+
+
+class ADS1232:
+    def __init__(self, power_down, clock, data_out):
+
+        self._power_down = digitalio.DigitalInOut(power_down)
+        self._power_down.direction = digitalio.Direction.OUTPUT
+        self._power_down.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+        self._clock = digitalio.DigitalInOut(clock)
+        self._clock.direction = digitalio.Direction.OUTPUT
+        self._clock.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+        self._data_out = digitalio.DigitalInOut(data_out)
+        self._data_out.direction = digitalio.Direction.INPUT
+
+    def reset(self):
+        print("Resetting!")
+        self._power_down.value = False
+        self._power_down.value = True
+
+    def raw_read(self):
+
+        print("Waiting for value to go high")
+        while self._data_out.value:
+            pass
+
+        print("Waiting for value to go low")
+        while not self._data_out.value:
+            pass
+
+        result = bytearray(3)
+
+        for i in range(0, 8):
+            self._clock.value = True
+            result[0] = result[0] | self._data_out.value << i
+            self._clock.value = False
+
+        for i in range(0, 8):
+            self._clock.value = True
+            result[1] = result[1] | self._data_out.value << i
+            self._clock.value = False
+
+        for i in range(0, 8):
+            self._clock.value = True
+            result[2] = result[2] | self._data_out.value << i
+            self._clock.value = False
+
+        return int.from_bytes(result, "big", True)
+
