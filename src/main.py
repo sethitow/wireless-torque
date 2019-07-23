8c1de92a8a8ccd87e93d734a1b739f069992c909
Seth Itow <11025699+sethitow@users.noreply.github.com>
2019-07-23 00:28:50 -0700

    Working example of both drivers

--- src/main.py
+++ src/main.py
@@ -2,9 +2,53 @@ import time
 import board
 import busio
 import adafruit_adxl34x
+import ads1232
+import digitalio
 
+print("Running!")
+
+pin_data_out = board.MISO
+pin_clock = board.SCK
+pin_power_down = board.D10
+pin_speed = board.D9
+pin_temp = board.D11
+pin_mux_select = board.D12
+pin_gain1 = board.D5
+pin_gain2 = board.D6
+
+speed = digitalio.DigitalInOut(pin_speed)
+speed.direction = digitalio.Direction.OUTPUT
+speed.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+temp = digitalio.DigitalInOut(pin_temp)
+temp.direction = digitalio.Direction.OUTPUT
+temp.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+mux_select = digitalio.DigitalInOut(pin_mux_select)
+mux_select.direction = digitalio.Direction.OUTPUT
+mux_select.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+gain1 = digitalio.DigitalInOut(pin_gain1)
+gain1.direction = digitalio.Direction.OUTPUT
+gain1.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+gain2 = digitalio.DigitalInOut(pin_gain2)
+gain2.direction = digitalio.Direction.OUTPUT
+gain2.drive_mode = digitalio.DriveMode.PUSH_PULL
+
+speed.value = False
+temp.value = False
+mux_select.value = False
+gain1.value = True
+gain2.value = True
+
+
+adc = ads1232.ADS1232(pin_power_down, pin_clock, pin_data_out)
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
+adc.reset()
+
 while True:
     print("%f %f %f" % accelerometer.acceleration)
+    print(adc.raw_read())
     time.sleep(1)
